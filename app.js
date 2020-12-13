0f8e1f2aed87f7bd06fb8c20cac55cf3a8d6629a
Adam DuQuette <duquetteadam@gmail.com>
2020-12-12 16:10:33 -0800

    final lecute commit

--- app.js
+++ app.js
@@ -3,25 +3,30 @@ console.log("hello, world");
 var myVariableName = "Hello, World!";
 console.log(myVariableName); //excpeted output: Hello, World!
 
-// myVariableName = "no, goodbye world";
-// console.log(myVariableName); //expected output: no, goodbye world 
+myVariableName = "no, goodbye world";
+console.log(myVariableName); //expected output: no, goodbye world 
 
-// var promptQuestion = "What is the best starter pokemon?";
-// var response = prompt(promptQuestion);
-// console.log(response);
+var promptQuestion = "What is the best starter pokemon?";
+var response = prompt(promptQuestion);
+console.log(response);
 
-// var alertMessage;
+var alertMessage;
+var color;// declare variable
 
-// if (response === "Squirtle") {
-//     alertMessage = "Excellent choice! Squitle is good for your soul.";
-// } else if (response === "Bulbasaur") {
-//     alertMessage = "I mean...ok, sure.";
-// } else {
-//     alertMessage = "No, you are wrong.";
-// }
+if (response === "Squirtle") {
+    alertMessage = "Excellent choice! Squitle is good for your soul.";
+    color = "#00FF00"; //assign variable a value
+} else if (response === "Bulbasaur") {
+    alertMessage = "I mean...ok, sure.";
+    color = "#FF0000";
+} else {
+    color = "#FF0000";
+    alertMessage = "No, you are wrong.";
+}
 
-// alert(alertMessage);
+alert(alertMessage);
 
-// var element = document.getElementById('response-output')
+var element = document.getElementById('response-output')
 
-// element.innerText = alertMessage;
+element.innerText = alertMessage;
+element.style.color = color; // use variable
