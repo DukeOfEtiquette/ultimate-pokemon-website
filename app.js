839feadda9f3f822e6f74d67bca1703b8232933b
Adam DuQuette <duquetteadam@gmail.com>
2020-12-15 13:30:32 -0800

    organize code with all function declarations at the top and each function call in logical order

--- app.js
+++ app.js
@@ -1,59 +1,11 @@
 'use strict';
 
-var response;
-
-function getUserPrompt(){
-    // Create a message variable to be used as an ARGUMENT for the prompt() function
-    var promptQuestion = "What is the best starter pokemon?";
-    response = prompt(promptQuestion);
-    response = response.toLowerCase();
-    console.log(response);
-}
-
-getUserPrompt();
-
-// parameter: name of variable to be used INSIDE the function
-function sayMessage(msg) {
-    alert(msg); // msg is an argument for alert()
-}
-
-// sayMessage("Hello from the sayMessage() function");
-
-function add(a, b) {
-    var result = a + b; //some work, could be anything!
-    return result; //end the function with a return call
-}
-
-var foo = add(5, 2);
-
-var color;
-
-function setColorBasedOnUserInput(rsp) {
-
-    // 
-    if (rsp === "squirtle") {
-        color = "lightblue";
-    } else if (rsp === "bulbasaur") {
-        color = "lightgreen";
-    } else if (rsp === "charmander") {
-        color = "coral";
-    } else {
-        color = "grey";
-        rsp = "Unown"
-    }
-    
-    // Update background color
-    document.getElementById("content").style.backgroundColor = color;
-}
-
-setColorBasedOnUserInput(response);
-
-
 // D. R. Y.
 // Don't
 // Repeat
 // Yourself
 
+/* ################# OLD CODE ################ */
 // Add Squirtle
 // document.write("<section> \
 //                     <h2>Squirtle</h2> \
@@ -75,6 +27,7 @@ setColorBasedOnUserInput(response);
 //                     <p>Charmander is the coolest!</p> \
 //                 </section>");
 
+/* ############## REFACTORED CODE ############# */
 function addPokemonToDocument(pokemonName, description) {
     if(pokemonName === "bulbasaur"){
         description = "Some unique desc for bulbasaur";
@@ -87,12 +40,50 @@ function addPokemonToDocument(pokemonName, description) {
                     </section>");
 }
 
+function getUserPrompt(){
+    // Create a message variable to be used as an ARGUMENT for the prompt() function
+    var promptQuestion = "What is the best starter pokemon?";
+    response = prompt(promptQuestion);
+    response = response.toLowerCase();
+    console.log(response);
+}
+
+// parameter: name of variable to be used INSIDE the function
+function sayMessage(msg) {
+    alert(msg); // msg is an argument for alert()
+}
+
+function add(a, b) {
+    var result = a + b; //some work, could be anything!
+    return result; //end the function with a return call
+}
+
+function setColorBasedOnUserInput(rsp) {
+
+    if (rsp === "squirtle") {
+        color = "lightblue";
+    } else if (rsp === "bulbasaur") {
+        color = "lightgreen";
+    } else if (rsp === "charmander") {
+        color = "coral";
+    } else {
+        color = "grey";
+        rsp = "Unown"
+    }
+    
+    // Update background color
+    document.getElementById("content").style.backgroundColor = color;
+}
+
+// sayMessage("Hello from the sayMessage() function");
+var foo = add(5, 2);
+
+var response;
+getUserPrompt();
+
+var color;
+setColorBasedOnUserInput(response);
+
 addPokemonToDocument("squirtle", "is totally rad");
 addPokemonToDocument("bulbasaur", "is leafy");
 addPokemonToDocument("charmander", "is on fire");
-
-// if(response === "Squirtle"){
-//     addPokemonToDocument("Squirtle");
-// } else if(response === "Bulbasaur"){
-//     addPokemonToDocument("Bulbasaur");
-// }
