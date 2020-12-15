c24cfa24feb23382bd5caa5ad2c9dc5f161f6196
Adam DuQuette <duquetteadam@gmail.com>
2020-12-14 16:08:01 -0800

    place HTML strings in better format to increase readability

--- app.js
+++ app.js
@@ -18,13 +18,25 @@ if (response === "Squirtle") {
 }
 
 // Add Squirtle
-document.write("<section><h2>Squirtle</h2><img src=\"images/Squirtle.png\"><p>Squirtle is the coolest!</p></section>");
+document.write("<section> \
+                    <h2>Squirtle</h2> \
+                    <img src=\"images/Squirtle.png\"> \
+                    <p>Squirtle is the coolest!</p> \
+                </section>");
 
 // Add Bulbasaur
-document.write("<section><h2>Bulbasaur</h2><img src=\"images/Bulbasaur.png\"><p>Bulbasaur is the coolest!</p></section>");
+document.write("<section>\
+                    <h2>Bulbasaur</h2> \
+                    <img src=\"images/Bulbasaur.png\"> \
+                    <p>Bulbasaur is the coolest!</p> \
+                </section>");
 
 // Add Charmander
-document.write("<section><h2>Charmander</h2><img src=\"images/Charmander.png\"><p>Charmander is the coolest!</p></section>");
+document.write("<section> \
+                    <h2>Charmander</h2> \
+                    <img src=\"images/Charmander.png\"> \
+                    <p>Charmander is the coolest!</p> \
+                </section>");
 
 // Update background color
 document.getElementById("content").style.backgroundColor = color;
